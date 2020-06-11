f245fb09c6f9b675dd0010be4c10868486ca399a
Samip <samipgnyawali15@gmail.com>
2020-06-11 20:43:20 +0545

    Added Async Task Loader

--- app/src/main/java/com/example/stromful/MainActivity/MainActivity.java
+++ app/src/main/java/com/example/stromful/MainActivity/MainActivity.java
@@ -1,23 +1,25 @@
 package com.example.stromful.MainActivity;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.AsyncTaskLoader;
+import androidx.loader.content.Loader;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.stromful.Data.StromfulPrefrences;
 import com.example.stromful.DetailActivity;
@@ -25,18 +27,20 @@ import com.example.stromful.ForcastAdapter;
 import com.example.stromful.R;
 import com.example.stromful.Utilities.NetworkUtils;
 import com.example.stromful.Utilities.OpenWeatherJsonWeather;
+import com.example.stromful.Utilities.StromfulWeatherUtils;
 import com.github.ybq.android.spinkit.SpinKitView;
 
 import java.net.URL;
 
-public class MainActivity extends AppCompatActivity implements ForcastAdapter.ForcastAdapterOnclickListner {
+public class MainActivity extends AppCompatActivity implements ForcastAdapter.ForcastAdapterOnclickListner, LoaderManager.LoaderCallbacks<String[]> {
+    private static final int FORCAST_LOADER_ID = 0;
     private static final String TAG = "com/example/stromful/MainActivity";
+    private static final String LOCATION_QUERY = "location";
     private TextView mErrorMessageDisplay;
     private SpinKitView mLoadingindicator;
     RecyclerView mrecycler;
     private ForcastAdapter mForcastAdapter;
     private Context mContext = MainActivity.this;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,7 +48,6 @@ public class MainActivity extends AppCompatActivity implements ForcastAdapter.Fo
         mrecycler = findViewById(R.id.recycler_view_forcast);
         mErrorMessageDisplay = findViewById(R.id.tv_error_message);
         mLoadingindicator = findViewById(R.id.progress_bar_laoding_indicator);
-
         //set rv
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         mrecycler.setLayoutManager(linearLayoutManager);
@@ -53,11 +56,30 @@ public class MainActivity extends AppCompatActivity implements ForcastAdapter.Fo
         mForcastAdapter = new ForcastAdapter(this);
         mrecycler.setAdapter(mForcastAdapter);
         loadWeateerData();
+        //initilizing the loader
+        getSupportLoaderManager().initLoader(FORCAST_LOADER_ID,null,this);
     }
+
+    /**
+     * Making location to fetch the data from giving to loder menager with bundle
+     */
 
     private void loadWeateerData() {
         String location = StromfulPrefrences.getPreferedWeatherLocation(this);
-        new FetchWeather().execute(location);
+        Bundle queryBundle = new Bundle();
+        queryBundle.putString(LOCATION_QUERY,location);
+        //implementing the loader manager
+        LoaderManager loaderManager = getSupportLoaderManager();
+        //create a loader
+        Loader<String> locationSerchLoader = loaderManager.getLoader(FORCAST_LOADER_ID);
+        if (locationSerchLoader==null){
+            //no data is need to be added to loader
+            loaderManager.initLoader(FORCAST_LOADER_ID,queryBundle,this);
+        }else{
+            //new data need to be loaded to the loader
+            loaderManager.restartLoader(FORCAST_LOADER_ID,queryBundle,this);
+        }
+
     }
 
     private void showWeatherDataView() {
@@ -73,52 +95,94 @@ public class MainActivity extends AppCompatActivity implements ForcastAdapter.Fo
     @Override
     public void onClick(String weatherfordy) {
         Intent intent = new Intent(mContext, DetailActivity.class);
-        intent.putExtra(Intent.EXTRA_TEXT,weatherfordy);
+        intent.putExtra(Intent.EXTRA_TEXT, weatherfordy);
         startActivity(intent);
     }
 
-    /* --------------------------- Async Task---------------------
-     * */
+    //-------------------------Loader Manager Function---------------------------
 
-    public class FetchWeather extends AsyncTask<String, Void, String[]> {
+    /**
+     * Creating a async task to fetch the data
+     * @param id
+     * @param args
+     * @return
+     */
 
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            mLoadingindicator.setVisibility(View.VISIBLE);
-        }
-
-        @SuppressLint("LongLogTag")
-        @Override
-        protected String[] doInBackground(String... params) {
-            if (params.length == 0) {
-                return null;
+    @NonNull
+    @Override
+    public Loader<String[]> onCreateLoader(int id, @Nullable final Bundle args) {
+        //create a async task loader
+        return  new AsyncTaskLoader<String[]>(this) {
+            String[] mWeatherData=null;
+            @Override
+            protected void onStartLoading() {
+                if (args==null){
+                    return;
+                }
+                if (mWeatherData!=null){
+                    deliverResult(mWeatherData);
+                }
+                mLoadingindicator.setVisibility(View.VISIBLE);
+                // triggers the load in background function to load data
+                forceLoad();
             }
-            String location = params[0];
-            URL weatherrequestUrl = NetworkUtils.buildUrl(location);
-            try {
-                String jsonWeatherResponce = NetworkUtils.getResponseFromHttpUrl(weatherrequestUrl);
-                String[] weatherDataFromJson = OpenWeatherJsonWeather.getWeatherDataFromJson(MainActivity.this, jsonWeatherResponce);
-                Log.e(TAG, "The Weather Data Is" + weatherDataFromJson[0]);
-                return weatherDataFromJson;
-            } catch (Exception e) {
-                e.printStackTrace();
-                return null;
-            }
-        }
 
-        @Override
-        protected void onPostExecute(String[] weatherdata) {
-            mLoadingindicator.setVisibility(View.INVISIBLE);
-            if (weatherdata != null) {
-                showWeatherDataView();
-                mForcastAdapter.setWeatherData(weatherdata);
+            @SuppressLint("LongLogTag")
+            @Nullable
+            @Override
+            public String[] loadInBackground() {
 
-            } else {
-                showErrorMessage();
+                String location = StromfulPrefrences.getPreferedWeatherLocation(mContext);
+                URL weatherRequestUrl = NetworkUtils.buildUrl(location);
+                try{
+                    String jsonWeatherResponse = NetworkUtils.getResponseFromHttpUrl(weatherRequestUrl);
+                    String[] weatherDataFromJson = OpenWeatherJsonWeather.getWeatherDataFromJson(MainActivity.this,jsonWeatherResponse);
+                    Log.e(TAG,"The weather data is " + weatherDataFromJson.length + weatherDataFromJson);
+                    return weatherDataFromJson;
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                    return null;
+                }
+
             }
+
+            @Override
+            public void deliverResult(@Nullable String[] mLocationJson) {
+                super.deliverResult(mLocationJson);
+            }
+        };
+    }
+
+    /**
+     * This function is executed after data is fetched
+     * @param loader
+     * @param weatherdata
+     */
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<String[]> loader, String[] weatherdata) {
+
+        mLoadingindicator.setVisibility(View.INVISIBLE);
+        if (weatherdata != null) {
+            showWeatherDataView();
+            mForcastAdapter.setWeatherData(weatherdata);
+
+        } else {
+            showErrorMessage();
         }
     }
+
+    /**
+     * This function Helps to reset the loader
+     * @param loader
+     */
+    @Override
+    public void onLoaderReset(@NonNull Loader<String[]> loader) {
+
+    }
+
 
     /* --------------------------- menue---------------------
      * */
@@ -137,7 +201,7 @@ public class MainActivity extends AppCompatActivity implements ForcastAdapter.Fo
             loadWeateerData();
             return true;
         }
-        if (id==R.id.action_map){
+        if (id == R.id.action_map) {
             openLocationInMap();
             return true;
         }
@@ -147,15 +211,12 @@ public class MainActivity extends AppCompatActivity implements ForcastAdapter.Fo
     /* ----------------------------intents
 
      */
-    private void openLocationInMap(){
+    private void openLocationInMap() {
         String address = StromfulPrefrences.getPreferedWeatherLocation(mContext);
-
-        Uri geoloaction = Uri.parse("geo:0,0?q="+address);
-        Intent intent = new Intent(Intent.ACTION_VIEW,geoloaction);
-        if (intent.resolveActivity(getPackageManager())!=null){
+        Uri geoloaction = Uri.parse("geo:0,0?q=" + address);
+        Intent intent = new Intent(Intent.ACTION_VIEW, geoloaction);
+        if (intent.resolveActivity(getPackageManager()) != null) {
             startActivity(intent);
         }
-
-
     }
 }
